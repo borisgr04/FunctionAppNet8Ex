26d2e0b90e62165d38cf64171322c51d91546ca2
Boris Arturo González Rivera <Bgonzalez@intergrupo.com>
2024-09-18 20:05:23 -0500

    se agrega el swagger - OpenApi

--- Function1.cs
+++ Function1.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace FunctionAppNet8Ex
 {
@@ -15,6 +19,9 @@ namespace FunctionAppNet8Ex
         }
 
         [Function("Function1")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The name parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
@@ -8,7 +12,41 @@ var host = new HostBuilder()
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddSingleton<IOpenApiConfigurationOptions>(_ =>
+        {
+            var options = new OpenApiConfigurationOptions()
+            {
+                Info = new OpenApiInfo()
+                {
+                    Version = "1.0.0",
+                    Title = "Swagger Petstore",
+                    Description = "This is a sample server Petstore API designed by [http://swagger.io](http://swagger.io).",
+                    TermsOfService = new Uri("https://github.com/Azure/azure-functions-openapi-extension"),
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "Enquiry",
+                        Email = "azfunc-openapi@microsoft.com",
+                        Url = new Uri("https://github.com/Azure/azure-functions-openapi-extension/issues"),
+                    },
+                    License = new OpenApiLicense()
+                    {
+                        Name = "MIT",
+                        Url = new Uri("http://opensource.org/licenses/MIT"),
+                    }
+                },
+                Servers = DefaultOpenApiConfigurationOptions.GetHostNames(),
+                OpenApiVersion = OpenApiVersionType.V2,
+                IncludeRequestingHostName = true,
+                ForceHttps = false,
+                ForceHttp = false,
+            };
+
+            return options;
+        });
     })
     .Build();
 
 host.Run();
+
+
+//https://github.com/Azure/azure-functions-openapi-extension/blob/main/docs/openapi-out-of-proc.md
